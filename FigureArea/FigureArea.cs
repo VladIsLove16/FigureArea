357ac93cdbc1983afa70a42631a57e4157e5623b
Vladislove <97838524+VladIsLove16@users.noreply.github.com>
2024-04-09 01:11:26 +0300

    CircleSquare

--- FigureArea/FigureArea.cs
+++ FigureArea/FigureArea.cs
@@ -2,6 +2,9 @@
 {
     public static class FigureArea
     {
-
+       public static float CircleSquare(float radius)
+        {
+            return MathF.Pow(radius, 2) * (float) Math.PI;
+        }
     }
 }
