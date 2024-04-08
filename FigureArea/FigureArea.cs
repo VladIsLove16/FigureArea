59a21e0351bf8b01add2bc69e52c4d7022f268a8
Vladislove <97838524+VladIsLove16@users.noreply.github.com>
2024-04-09 01:17:56 +0300

    TriangleSquare. Без учёта всякого вида исключений ( требуется корректный ввод)

--- FigureArea/FigureArea.cs
+++ FigureArea/FigureArea.cs
@@ -6,5 +6,13 @@
         {
             return MathF.Pow(radius, 2) * (float) Math.PI;
         }
+        public static float TriangleSquare(float a, float b, float c)
+        {
+            float P = a + b + c;
+            float p = P / 2;
+            float SquareS = p*(p - a) * (p - b)*(p - c);
+            float S=(float)Math.Sqrt(SquareS);
+            return S;
+        }
     }
 }
