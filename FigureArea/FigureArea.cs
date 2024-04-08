38075fc0a453259c9f25d6276c51167f87799b44
Vladislove <97838524+VladIsLove16@users.noreply.github.com>
2024-04-09 01:48:18 +0300

    С обработкой исключений и
    На превосходство 3й стороны по длине и на ненулевую длину сторон

--- FigureArea/FigureArea.cs
+++ FigureArea/FigureArea.cs
@@ -4,10 +4,23 @@
     {
        public static float CircleSquare(float radius)
         {
+            if (radius<0)
+                throw new ArgumentException("Длина радиуса должна быть положительна");
             return MathF.Pow(radius, 2) * (float) Math.PI;
         }
         public static float TriangleSquare(float a, float b, float c)
         {
+            if( a<= 0 || b<=0 || c <=0)
+                throw new ArgumentException("Все стороны должны иметь положительную длину");
+            char flag = ' ';
+            if ((a + b) <= c)
+                flag = 'c';
+            else if ((a + c) <= b)
+                flag = 'b';
+            else if ((b + c) <= a)
+                flag = 'a';
+            if (flag != ' ')
+                throw new ArgumentException($"Сторона {flag} больше суммы двух других");
             float P = a + b + c;
             float p = P / 2;
             float SquareS = p*(p - a) * (p - b)*(p - c);
