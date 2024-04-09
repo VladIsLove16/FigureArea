80604d6d3c997366dc17d01b61606362628645de
Vladislove <97838524+VladIsLove16@users.noreply.github.com>
2024-04-09 15:31:35 +0300

    Unit Tests

---
+++ Test/Program.cs
@@ -0,0 +1,40 @@
+﻿using FigureArea;
+using Xunit;
+float a;
+
+public class CircleSquareTest()
+{
+    private float negativeException=-1;
+    private float notException =0.6f;//s=
+    [Fact]
+    public void NegativeException()
+    {
+        Assert.Throws<ArgumentException>(() => FigureArea.FigureArea.CircleSquare(negativeException));
+    }
+    [Fact]
+    public void Normal()
+    {
+        Assert.InRange(FigureArea.FigureArea.CircleSquare(notException), 1.13, 1.14);
+    }
+}
+public class TriangleSquareTest()
+{
+    private float[] nullException = [0, 0.5f, 0.6f];
+    private float[] negativeException = [-1, 0.5f, 0.6f];
+    private float[] notException = [1f, 0.5f, 0.6f];//s=
+    [Fact]
+    public void NullException()
+    {
+        Assert.Throws<ArgumentException>(() => FigureArea.FigureArea.TriangleSquare(nullException[0], nullException[1], nullException[2]));
+    }
+    [Fact]
+    public void NegativeException()
+    {
+        Assert.Throws<ArgumentException>(() => FigureArea.FigureArea.TriangleSquare(negativeException[0], negativeException[1], negativeException[2]));
+    }
+    [Fact]
+    public void Normal()
+    {
+        Assert.InRange(FigureArea.FigureArea.TriangleSquare(notException[0], notException[1], notException[2]), 0.11, 0.12);
+    }
+}
