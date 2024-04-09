fbb0f76f3281411cc617982437e6b146b70b0c17
Vladislove <97838524+VladIsLove16@users.noreply.github.com>
2024-04-09 16:31:34 +0300

    Добавление классов для фигур
    Изначально я не думал создавать для каждого вида фигуры свой класс, но для реализации "Вычисление площади фигуры без знания типа фигуры в compile-time" он требуется

---
+++ FigureArea/Circle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FigureArea
+{
+    public class Circle : IShape
+    {
+        public Circle(float radius) { 
+            this.radius = radius;
+        }
+        public float radius;
+
+        public float CalculateArea()
+        {
+            return FigureArea.CircleSquare(radius);
+        }
+    }
+}
---
+++ FigureArea/Triangle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FigureArea
+{
+    internal class Triangle :IShape
+    {
+        public Triangle(float a,float b, float c) {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+        public float a;
+        public float b;
+        public float c;
+
+        public float CalculateArea()
+        {
+            return FigureArea.TriangleSquare(a, b, c);
+        }
+    }
+}
